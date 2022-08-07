86b02404f4f14f96a7e8ceb9e5993cb40b882d50
e_ikuta <e_ikuta@ga-tech.co.jp>
2022-08-07 13:17:58 +0900

    .zshrc と .vimrc を追加するコマンド追加

--- script.sh
+++ script.sh
@@ -1,20 +1,32 @@
 #!/bin/bash
-set -eux
+set -eu
 
-####################
-# install packages
-####################
+echo '===== add .zshrc ====='
+ZSHRC=~/.zshrc
+if test -f "$ZSHRC"; then
+  echo "$ZSHRC exists."
+else
+  ORIGINAL_ZSHRC=`readlink -f .zshrc`
+  ln -s "$ORIGINAL_ZSHRC" "$ZSHRC"
+fi
+
+echo '===== add .vimrc ====='
+VIMRC=~/.vimrc
+if test -f "$VIMRC"; then
+  echo "$VIMRC exists."
+else
+  ORIGINAL_VIMRC=`readlink -f .vimrc`
+  ln -s "$ORIGINAL_VIMRC" "$VIMRC"
+fi
+
+echo '===== install packages ====='
 brew bundle
 
-##################
-# z command
-##################
+echo '===== install z command ====='
 cd ~
 git clone git@github.com:rupa/z.git
 mv z .zsh.d
 
-##################
-# vim plug
-##################
+echo '===== install vim plug ====='
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
