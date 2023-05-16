ff98320b484b40bbecc2a7ac3eed853674f2001c
e_ikuta <e_ikuta@ga-tech.co.jp>
2023-05-16 17:47:33 +0900

    README追加

---
+++ README.sh
@@ -0,0 +1,9 @@
+## セットアップ手順（macOSを想定）
+1. 使用中のPCでupdate.shを実行
+1. 使用中のPCでiterm2の設定をファイルに保存
+1. 新PCにiTerm2をブラウザからインストール
+1. 新PCにhomebrewをインストール
+1. homebrewを使用して、新PCにgitをインストール
+1. githubのセットアップを行う（https://docs.github.com/en/get-started/quickstart/set-up-git）
+1. gitを使用して、新PCに本リポジトリをclone
+1. 新PCでsetup.shを実行
