274fc3c655fc7416f48b0de6354df7cc68fb59c4
eminem <49850014+e-ikuta@users.noreply.github.com>
2023-10-06 15:22:25 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -31,3 +31,5 @@ mv z .zsh.d
 echo '===== install vim plug ====='
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+source "$ZSHRC"
